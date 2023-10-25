64a0efa41eb7b04279cee841bd0d53e5e0495e63
clar04 <123356941+clar04@users.noreply.github.com>
2023-10-25 10:50:38 +0700

    Update and rename Logs.ts to Log.ts - edit log

---
+++ src/collections/Log.ts
@@ -0,0 +1,49 @@
+import { CollectionConfig } from "payload/types";
+
+const Log: CollectionConfig = {
+  slug: "Log",
+  auth: false,
+  admin: {
+    useAsTitle: "action",
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: "action",
+      label: "Action",
+      type: "select",
+      options: ["Create", "Update", "Delete"],
+      required: true,
+    },
+    {
+      name: "itemType",
+      label: "Koleksi",
+      type: "select",
+      options: ["Todo", "Catagory"], 
+      required: true,
+    },
+    {
+      name: "priority",
+      label: "Priority",
+      type: "relationship",
+      relationTo: "Todo, Catagory", // Hubungan dengan collection "Todo" atau "Catagory"
+      required: true,
+    },
+    {
+      name: "dateOnly",
+      label: "Time Stamp",
+      type: "date",
+      admin: {
+        date: {
+          pickerAppearance: "dayOnly",
+          displayFormat: "d MMM yyyy, HH:mm",
+        },
+      },
+    },
+  ],
+};
+
+export default Log;
+
